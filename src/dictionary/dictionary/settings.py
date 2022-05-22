8e7ee3d0888a68f453845e0b1287699154930d1f
abhilash <abhilashazheekkal@gmail.com>
2022-05-22 15:58:13 +0530

    first commit

--- src/dictionary/dictionary/settings.py
+++ src/dictionary/dictionary/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[ BASE_DIR / "static"]
+# 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
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
